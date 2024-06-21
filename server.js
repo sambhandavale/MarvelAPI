a7a4736719d6739c0db33cbb1056cea32749309a
sambhandavale <sambhandavale15@gmail.com>
2024-06-21 19:45:38 +0530

    added cors

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import cors from 'cors';
+
 import showsRouter from './routes/shows.js';
 import moviesRouter from './routes/movies.js';
 
@@ -6,6 +8,7 @@ const app = express();
 const port = 5000;
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/api/shows', showsRouter);
 app.use('/api/movies', moviesRouter);
